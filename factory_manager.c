2768e33e9e8331f2bb64710a85f01e651e1c26c6
Jaime Sánchez <100522207@alumnos.uc3m.es>
2025-05-01 17:30:42 +0200

    CosasCambios lecturas

--- factory_manager.c
+++ factory_manager.c
@@ -1,60 +1,70 @@
 /*
- *
  * factory_manager.c
- *
  */
 
- #include <stdio.h>
- #include <stdlib.h>
- #include <unistd.h>
- #include <fcntl.h>
- #include <stddef.h>
- #include <semaphore.h>
- #include <sys/stat.h>
- 
- struct parametros{
-	 int id;
-	 int belt_size;
-	 int num_products;
- };
- 
- const int max_line = 1024;
- 
- int main (int argc, const char * argv[] ){
-	 if (argc != 2){
-		 fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
-		 return -1;
-	 }
- 
-	 int* status;
-	 int fd = open(argv[1], O_RDONLY); /*Abirmos el archivo del que leer la cadena*/
-	 if (fd == -1 ){
-		 fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
-		 return -1;
-	 }
- 
-	 char buffer[max_line]; /*Creo la cadena de caracteres buffer, de la cual leeremos los /n */
-	 ssize_t bytes_leidos;
-	 int contador_numeros = -2; /*Se inicializa en -2 para tener en cuenta el /n y el numero de los procces manager*/
-	 int procesos = 0; /*Numero de procces managers a crear*/
- 
-	 while ((bytes_leidos = read(fd, buffer, 1)) > 0) {
-		 contador_numeros ++;
-		 
-	 }
-	 if ((contador_numeros% 3) == 0){
-		 printf("correcto");
-		 procesos = contador_numeros/3;
-	 } else {
-		 return -1;
-	 };
- 
-	 struct parametros *lista_parametros = malloc(sizeof(struct parametros) * procesos); /*Asigno la momoria que voy a utilizazr para pasar los parametros*/
-	 pthread_t *threads = malloc(sizeof(pthread_t) * procesos);
- 
-	 
- 
- 
-	 return 0;
- }
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+
+struct parametros {
+    int id;
+    int belt_size;
+    int num_products;
+};
+
+
+#define MAX_LINE 1024
+
+int main(int argc, const char * argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
+        return -1;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
+        return -1;
+    }
+
+    char buffer[MAX_LINE];
+    ssize_t bytes_read = read(fd, buffer, MAX_LINE - 1);
+    if (bytes_read <= 0) {
+        fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
+        close(fd);
+        return -1;
+    }
+    buffer[bytes_read] = '\0'; /*Para que funcione el Strtok*/
+    close(fd);
+
+    /*++++++++++++++++++++Procesar números+++++++++++++++++(inspirado del tokenizar linea del scripter)*/
+
+    int numeros[MAX_LINE/4];    /*256 enteros*/
+    int countador = 0;
+    char *token = strtok(buffer, " \n");
+    while (token != NULL && countador < MAX_LINE/4) {
+        numeros[countador++] = atoi(token);
+        token = strtok(NULL, " \n");
+    }
+
+	/*++++++++++++++++++++++++++++++++++++++++Procesar números++++++++++++++++++++++++++++++++++++++*/
+
+    if (countador < 1 || (countador - 1) % 3 != 0) {
+        fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
+        return -1;
+    }
+
+    int max_cintas = numeros[0];
+    int num_cintas = (countador - 1) / 3;
+
+    if (num_cintas > max_cintas || max_cintas <= 0) {
+        fprintf(stderr, "[ERROR][factory_manager] Invalid file.\n");
+        return -1;
+    }
+
+    struct parametros *lista_parametros = malloc(sizeof(struct parametros) * num_cintas);
+    pthread_t *threads = malloc(sizeof(pthread_t) * num_cintas);
