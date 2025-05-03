03d9e3557a2063d02a73d6f895fb1455c8f727e0
Jaime Sánchez <100522207@alumnos.uc3m.es>
2025-05-03 14:14:57 +0200

    Queue

--- queue.c
+++ queue.c
@@ -4,41 +4,74 @@
 #include "queue.h"
 
 char *cola;
+
+int tamano = 0; /*Tamano de la cola*/
+int principio = 0; /*Posicion del primer elemento*/	
+int final = 0; /*Posicion para insertar el siguiente elemento*/
+int cont = 0; /*Numero de elementos en la cola*/
+
 //To create a queue
 int queue_init(int size){
-	cola = (char *) malloc(size);
-	if (!cola){
+	if (size <= 0) {
+		return -1; /*Manejo de errores*/
+	}
+    cola = (struct element*)malloc(sizeof(struct element) * size); /*Se reserva el espacio segun los n elmentos*/
+    if (!cola) {
 		return -1;
 	}
-	
-	return 0;
+
+    tamano = size;
+    principio = 0;
+    final = 0;
+    cont = 0;
+    return 0;
 }
 
 
 // To Enqueue an element
 int queue_put(struct element* x) {
-
+	if (queue_full()) {
+		return -1;
+	} /*Manejo de errores*/
+	
+	cola[final] = *x; /*Se inserta el elemento en la cola*/
+	final = (final + 1) % tamano; /*Se actualiza la posicion del final haciendo mod*/
+	cont++; /*Se aumenta el contador*/
 	return 0;
 }
 
 
 // To Dequeue an element.
 struct element* queue_get(void) {
-	return NULL;
+	if (queue_empty()) {
+		return NULL; /*Manejo de errores*/
+	}
+
+    struct element* item = &cola[principio];
+    principio = (principio + 1) % tamano;
+    cont--;
+    return item;
 }
 
 
 //To check queue state
 int queue_empty(void){
-	return 0;
+	return (cont == 0);
 }
 
 int queue_full(void){
-	return 0;
+	return (cont == tamano);
 }
 
 //To destroy the queue and free the resources
 int queue_destroy(void){
-	free(cola);
-	return 0;
+	if (cola) {
+		free(cola);
+	}
+    cola = NULL;
+    tamano = 0;
+    principio = 0;
+    final = 0;
+    cont = 0;
+    return 0;
 }
