cd4b3e782928c22e585f4dc15dc571077b4bb7a2
Jaime Sánchez <100522207@alumnos.uc3m.es>
2025-05-04 21:45:41 +0200

    Final

--- process_manager.c
+++ process_manager.c
@@ -1,270 +1,137 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <pthread.h>
-
 #include <semaphore.h>
-
 #include "process_manager.h"
-
 #include "queue.h"
 
-
-
 pthread_mutex_t mutex;
-
 pthread_cond_t not_full, not_empty;
 
-
-
 int produced_count = 0;
-
 int consumed_count = 0;
 
-
-
 void *producer(void *arg) {
-
     struct parametros *params = (struct parametros *)arg;
-
     int produced = 0;
-
     
-
-
+    /* Imprimir lo que va a producir*/
+    printf("[OK][producer] Producer with id %d will produce %d elements.\n", 
+           params->id, params->num_products);
     
-
     while (produced < params->num_products) {
-
         pthread_mutex_lock(&mutex);
-
         
-
-        while (produced_count != 0) { // espera a que se haya consumido el lote anterior
-
+        while (produced_count != 0) { /*espera a que se haya consumido el lote anterior*/
             pthread_cond_wait(&not_full, &mutex);
-
         }
-
         
-
         int lote = 0;
-
         
-
         while (lote < params->belt_size && produced < params->num_products) {
-
             struct element product;
-
             product.num_edition = produced;
-
             product.id_belt = params->id;
-
             product.last = (produced == params->num_products - 1) ? 1 : 0;
-
             
-
             queue_put(&product);
-
             
-
             printf("[OK][queue] Introduced element with id %d in belt %d.\n", product.num_edition, params->id);
-
             
-
             lote++;
-
             produced++;
-
             produced_count++;
-
         }
-
         
-
-        pthread_cond_signal(&not_empty); // avisa al consumidor que puede empezar
-
+        pthread_cond_signal(&not_empty); /*avisa al consumidor que puede empezar*/
         pthread_mutex_unlock(&mutex);
-
     }
-
     
-
     pthread_exit(NULL);
-
 }
-
-
 
 void *consumer(void *arg) {
-
     struct parametros *params = (struct parametros *)arg;
-
     int done = 0;
-
     
-
     while (!done) {
-
         pthread_mutex_lock(&mutex);
-
         
-
-        while (produced_count == 0) { // espera hasta que haya productos
-
+        while (produced_count == 0) { /* espera hasta que haya productos*/
             pthread_cond_wait(&not_empty, &mutex);
-
         }
-
         
-
         while (produced_count > 0) {
-
             struct element *product = queue_get();
-
             printf("[OK][queue] Obtained element with id %d in belt %d.\n", product->num_edition, params->id);
-
             
-
             if (product->last) {
-
                 done = 1;
-
             }
-
             
-
             produced_count--;
-
             consumed_count++;
-
         }
-
         
-
         consumed_count = 0;
-
-        pthread_cond_signal(&not_full); // avisa al productor que puede hacer otro lote
-
+        pthread_cond_signal(&not_full); /* avisa al productor que puede hacer otro lote */
         pthread_mutex_unlock(&mutex);
-
     }
-
     
-
     pthread_exit(NULL);
-
 }
 
-
-
 void *process_manager(void *arg) {
-
     struct parametros *p = arg;
-
     
-
-    // 1) Esperar la señal inicial del factory_manager
-
+    /*Semáforos para la sincronización y gestionar los prints*/
     sem_wait(&p->start_sem[p->index]);
-
     
-
-    // 2) Imprimir el mensaje de espera e inicializar estructuras
 
     printf("[OK][process_manager] Process_manager with id %d waiting to produce %d elements.\n",
-
            p->id, p->num_products);
-
     
-
-    // 3) Notificar al factory_manager que estamos listos
 
     sem_post(&p->ready_sem[p->index]);
-
     
-
-    // 4) Esperar la señal para comenzar la producción
 
     sem_wait(&p->start_sem[p->index]);
-
     
-
-    // 5) Inicializar mutex, condvars y belt
-
+    /* Inicializar mutex, condvars y belt */
     pthread_mutex_init(&mutex, NULL);
-
     pthread_cond_init(&not_full, NULL);
-
     pthread_cond_init(&not_empty, NULL);
-
     
-
     if (queue_init(p->belt_size) != 0) {
-
         fprintf(stderr, "[ERROR][process_manager] There was an error executing process_manager with id %d.\n", p->id);
-
         sem_post(&p->done_sem[p->index]);
-
         pthread_exit(NULL);
-
     }
-
     
-
     printf("[OK][process_manager] Belt with id %d has been created with a maximum of %d elements.\n",
-
            p->id, p->belt_size);
-
     
-
-    // 6) Crear hilos productor y consumidor
-
+    /* Crear hilos productor y consumidor */
     pthread_t prod, cons;
-
     pthread_create(&prod, NULL, producer, p);
-
     pthread_create(&cons, NULL, consumer, p);
-
     
-
-    // 7) Esperar a que terminen
-
+    /* Join hilos productor y consumidor */
     pthread_join(prod, NULL);
-
     pthread_join(cons, NULL);
-
     
-
     queue_destroy();
-
     
-
-    // 8) Imprimir mensaje de finalización
-
     printf("[OK][process_manager] Process_manager with id %d has produced %d elements.\n",
-
            p->id, p->num_products);
-
     
-
-    // 9) Notificar al factory_manager que ha terminado
-
+    /* Notificar al factory_manager que ha terminado */
     sem_post(&p->done_sem[p->index]);
-
     
-
-    // 10) Limpiar recursos
-
+    /*Limpiar recursos*/
     pthread_mutex_destroy(&mutex);
-
     pthread_cond_destroy(&not_full);
-
     pthread_cond_destroy(&not_empty);
-
     
-
     pthread_exit(NULL);
-
 }
--- process_manager.h
+++ process_manager.h
@@ -1,36 +1,18 @@
 #ifndef PROCESS_MANAGER_H
-
 #define PROCESS_MANAGER_H
-
-
 
 #include <semaphore.h>
 
-
-
 struct parametros {
-
     int id;
-
     int belt_size;
-
     int num_products;
-
-
-
     int index;               // Índice del proceso
-
     sem_t *start_sem;        // Semáforos para arrancar procesos
-
     sem_t *done_sem;         // Semáforos para indicar que han terminado
-
+    sem_t *ready_sem;        // Semáforos para indicar que están listos para producir
 };
-
-
 
 void* process_manager(void* arg);
 
-
-
 #endif
-
